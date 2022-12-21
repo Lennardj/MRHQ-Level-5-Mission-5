8f11cbd9f03c795f90057a2bdfbdd3d5ad96eb01
Lennardj <lennardvincentjohn@gmail.com>
2022-12-21 21:41:35 +1300

    Docker Compose working with volumes for MongoDB

--- backend/DB/index.js
+++ backend/DB/index.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/", { useNewUrlParser: true })
+  .then(() => {
+    console.log("Mongodb connected....");
+  })
   .catch((e) => {
     console.error("Connection error", e.message);
   });
