80ff54c0583cfdb3754d000dc04104e93483181b
Lennardj <lennardvincentjohn@gmail.com>
2022-12-21 20:27:55 +1300

    created docker file and docker compose file

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const db = require("./db");
 require("dotenv").config();
 const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
