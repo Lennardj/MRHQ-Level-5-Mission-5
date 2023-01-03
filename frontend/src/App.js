5ffba4aa55314f6d0650118053ce1af9c1281be5
Lennardj <lennardvincentjohn@gmail.com>
2023-01-03 19:23:16 +1300

    Header and footer completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/header";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 import "./App.css";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Header />} />
+        <Route path="/1" element={<Footer />} />
       </Routes>
     </div>
   );
